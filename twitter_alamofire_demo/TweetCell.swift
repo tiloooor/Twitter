24d74c90b061cc5123b3fed14d64244a8371f232
Tyler Holloway <htoyll@fb.com>
2017-07-03 17:38:06 -0700

    like counter

--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -10,13 +10,53 @@ import UIKit
 
 class TweetCell: UITableViewCell {
     
+    // MARK: Properties
     @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var aliasLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var retweetLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeCount: UILabel!
+    @IBOutlet weak var replyButton: UIButton!
+    
+    var favoriteCount: Int?
     
     var tweet: Tweet! {
         didSet {
             tweetTextLabel.text = tweet.text
+            favoriteCount = tweet.favoriteCount!
         }
     }
+    
+    
+    @IBAction func pressRetweet(_ sender: Any) {
+        if retweetButton.isSelected {
+            retweetButton.isSelected = false
+        } else {
+            retweetButton.isSelected = true
+        }
+    }
+    
+    @IBAction func pressLike(_ sender: Any) {
+        tweet.favoriteCount = 0
+        if likeButton.isSelected {
+            likeButton.isSelected = false
+            self.favoriteCount! -= 1
+            self.likeCount.text = String(self.favoriteCount!)
+        } else {
+            likeButton.isSelected = true
+            self.favoriteCount! += 1
+            self.likeCount.text = String(self.favoriteCount!)
+        }
+    }
+   
+    
+    
+   
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
